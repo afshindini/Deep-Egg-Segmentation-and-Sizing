3024281263cef18a06f9b291a0124438611702ec
Afshin Dini <afshin_dini@yahoo.com>
2025-03-23 18:09:43 +0200

    Initialize the project

---
+++ src/egg_segmentation_size/main.py
@@ -0,0 +1,30 @@
+"""Run the main code for egg-segmentation-size"""
+
+import logging
+
+import click
+
+from egg_segmentation_size import __version__
+from egg_segmentation_size.logging import config_logger
+
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.version_option(version=__version__)
+@click.option("-l", "--log_path", type=str, help="Path to save log file")
+@click.option("-v", "--verbose", count=True, help="Shorthand for info/debug/warning/error loglevel (-v/-vv/-vvv/-vvvv)")
+def egg_segmentation_size_cli(log_path: str, verbose: int) -> None:
+    """This repo segments the eggs in images and gives an estimation for their sizes """
+    if verbose == 1:
+        log_level = 10
+    elif verbose == 2:
+        log_level = 20
+    elif verbose == 3:
+        log_level = 30
+    else:
+        log_level = 40
+    config_logger(log_level, log_path)
+
+    click.echo("Run the main code.")
